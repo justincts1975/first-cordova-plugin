4561573496e893d0c3b734d91bf4ddd15c0db694
Justin Greenwood <justin@cts-software.com>
2021-04-15 18:10:21 -0400

    Update FirstCordovaPlugin.java

--- src/android/com/example/FirstCordovaPlugin.java
+++ src/android/com/example/FirstCordovaPlugin.java
@@ -16,13 +16,13 @@ import android.util.Log;
 
 import java.util.Date;
 
-public class MyCordovaPlugin extends CordovaPlugin {
-  private static final String TAG = "MyCordovaPlugin";
+public class FirstCordovaPlugin extends CordovaPlugin {
+  private static final String TAG = "FirstCordovaPlugin";
 
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
 
-    Log.d(TAG, "Initializing MyCordovaPlugin");
+    Log.d(TAG, "Initializing FirstCordovaPlugin");
   }
 
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
